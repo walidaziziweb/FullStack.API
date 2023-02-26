1ab7b404f15316c4c149797605bc1fd39d6c35a2
walid azizi <fullstackwebdeveloper@walidazizi.com>
2023-02-26 11:12:23 +0100

    Mon premier commit

---
+++ FullStack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,82 @@
+﻿using FullStack.API.Data;
+using FullStack.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+        // Get All Employees
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _fullStackDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+        // Get Single a Employee
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetEmployees")]
+        public async Task<ActionResult> GetEmployees([FromRoute] Guid id)
+        {
+            var employees = await _fullStackDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (employees != null)
+            {
+                return Ok(employees);
+            }
+            return NotFound("Employee Not found");
+        }
+        // Add a Employee
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employees)
+        {
+            employees.Id = Guid.NewGuid();
+            await _fullStackDbContext.Employees.AddAsync(employees);
+            await _fullStackDbContext.SaveChangesAsync();
+          return CreatedAtAction(nameof(GetEmployees), new { id = employees.Id }, employees);
+        }
+        // Update a Employee
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployees([FromRoute] Guid id, [FromBody] Employee employees)
+        {
+           // var existingEmployee = await _fullStackDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            var existingEmployee = await _fullStackDbContext.Employees.FindAsync(id);
+            if (existingEmployee != null)
+            {
+                existingEmployee.Name = employees.Name;
+                existingEmployee.Phone = employees.Phone;
+                existingEmployee.Email = employees.Email;
+                existingEmployee.Salary = employees.Salary;
+                existingEmployee.Department = employees.Department;
+                await _fullStackDbContext.SaveChangesAsync();
+                return Ok(existingEmployee);
+            }
+            return NotFound("Employee not found");
+        }
+        // Delete a Employee
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            _fullStackDbContext.Employees.Remove(employee);
+            await _fullStackDbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
+    }
+}
---
+++ FullStack.API/Program.cs
@@ -0,0 +1,35 @@
+using FullStack.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FullStackDbContext>(
+    o => o.UseSqlServer(builder.Configuration.GetConnectionString("FullStackDbConnectionStrings")));
+var app = builder.Build();
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//app.UseCors("default");
+
+app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
